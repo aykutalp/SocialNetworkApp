c01416a337432a20b29d74bc0bb4d0fe511c3897
aykutalp <aykutalps@msn.com>
2019-06-09 21:41:57 +0100

    Add Token Authentication

--- SocialNetworkApp.API/Controllers/ValuesController.cs
+++ SocialNetworkApp.API/Controllers/ValuesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SocialNetworkApp.API.Data;
 
 namespace SocialNetworkApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -28,7 +30,7 @@ namespace SocialNetworkApp.API.Controllers
             return Ok(values);
         }
 
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
---
+++ SocialNetworkApp.API/DTOs/UserForLoginDto.cs
@@ -0,0 +1,9 @@
+namespace SocialNetworkApp.API.DTOs
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+    }
+}

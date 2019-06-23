47ed57d03eac365d788ed69e9bd84a5773282f2a
aykutalp <aykutalps@msn.com>
2019-06-23 22:16:13 +0100

    Add Basic Register Form

--- ClientApp/src/app/nav/nav.component.ts
+++ ClientApp/src/app/nav/nav.component.ts
@@ -21,4 +21,14 @@ export class NavComponent implements OnInit {
       console.log('Failed to login');
     });
   }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token; // !! returns true or false
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    console.log('Logged out');
+  }
 }

c61c88b965e690b3654bb8b15d7bf084881763d6
aykutalp <aykutalps@msn.com>
2019-07-11 22:03:22 +0100

    Add Routing and Route Protection

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
+
+import { AuthService } from './services/auth.service';
+import { ErrorInterceptorProvider } from './services/error.interceptor';
+import { appRoutes } from './routes';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
-
-import { AuthService } from './services/auth.service';
-import { ErrorInterceptorProvider } from './services/error.interceptor';
-
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-
-
+import { FriendsComponent } from './friends/friends.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
 
 
 @NgModule({
@@ -23,13 +25,17 @@ import { RegisterComponent } from './register/register.component';
       ValueComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      FriendsComponent,
+      MessagesComponent,
+      ListsComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
